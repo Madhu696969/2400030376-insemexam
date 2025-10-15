e59069df5f23c7db521f1cc85185613f3826bd73
KaleruMadhu <2400030376@kluniversity.in>
2025-10-15 17:10:11 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Product from './Product'
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Product />
-  </StrictMode>,
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/Product" element={<Product/>}/>
+    </Routes>
+
+  </Router>
+  
 )
